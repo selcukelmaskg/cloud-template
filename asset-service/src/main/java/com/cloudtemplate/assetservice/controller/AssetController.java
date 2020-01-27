8ab4235f162aab92412c2830809f22eacc41d17d
bilalkocoglu <bilal.kocoglu@kgteknoloji.com>
2020-01-27 14:08:54 +0300

    simple api descriptions in asset service

--- asset-service/src/main/java/com/cloudtemplate/assetservice/controller/AssetController.java
+++ asset-service/src/main/java/com/cloudtemplate/assetservice/controller/AssetController.java
@@ -3,6 +3,8 @@ package com.cloudtemplate.assetservice.controller;
 import com.cloudtemplate.assetservice.domain.Asset;
 import com.cloudtemplate.assetservice.service.AssetService;
 import com.cloudtemplate.shared.dto.asset.AssetBindingRequest;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.junit.Assert;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(AssetController.END_POINT)
+@Api(value = "Asset Controller", description = "Asset CRUD operations")
 public class AssetController {
     static final String END_POINT = "/api/asset";
 
@@ -25,16 +28,19 @@ public class AssetController {
         this.assetService = assetService;
     }
 
+    @ApiOperation(value = "IP address provider in order fulfilment", response = String.class)
     @PostMapping("/binding")
     public String assetBinding(@RequestBody @Valid AssetBindingRequest assetBindingRequest) {
         return assetService.ipBinding(assetBindingRequest.getOrderId());
     }
 
+    @ApiOperation(value = "IP address list", response = List.class)
     @GetMapping
     public List<Asset> findAll() {
         return assetService.findAll();
     }
 
+    @ApiOperation(value = "Find Asset by order ID")
     @GetMapping("/order/{orderId}")
     public Asset findByOrderId(@PathVariable(name = "orderId") String orderId) {
         Assert.assertFalse("OrderId must be not null !", orderId.isEmpty());
